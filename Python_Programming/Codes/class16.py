eb8d725d01b7fb1c719f90c1aa9b2b705c77c68c
ankitchoudhary-github <itschoudharyankit@gmail.com>
2026-08-24 10:52:49 +0530

    changed .py to jypiter notebooks

--- Python_Programming/Codes/class16.py
+++
@@ -1,9 +0,0 @@
-# [1, [2, 3], [4, [5]]] -> find the sum of all elements in the nested list
-def sum_nested_list(nested_list):
-    total = 0
-    for element in nested_list:
-        if isinstance(element, list):
-            total += sum_nested_list(element)  # Recursively sum nested lists
-        else:
-            total += element  # Add the integer value
-    return total
